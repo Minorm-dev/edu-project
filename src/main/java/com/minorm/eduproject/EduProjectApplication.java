2b2194a25d01b7f13ad7810a443cb819aeec1b3f
Minorm-dev <maxim.san231@gmail.com>
2025-05-04 21:40:09 +0300

    base

---
+++ src/main/java/com/minorm/eduproject/EduProjectApplication.java
@@ -0,0 +1,13 @@
+package com.minorm.eduproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EduProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/minorm/eduproject/EduProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minorm.eduproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduProjectApplicationTests {
+
+//    @Test
+//    void contextLoads() {
+//    }
+
+}
